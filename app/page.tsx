87aa51ee2bc754e783bc826352a5f71141a7b340
Carlos Arevalo <carlosandresat@hotmail.com>
2025-07-17 16:56:54 -0500

    feat: add ModeToggle component for light/dark themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return <h1>Hello, Yachay Legend!</h1>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-2">
+      <ModeToggle />
+      <Button size="lg">Grupo A</Button>
+      <Button size="lg">Grupo B</Button>
+      <Button size="lg">Grupo C</Button>
+      <Button size="lg">Grupo D</Button>
+    </div>
+  )
 }
