a4a56774bcda2063a637dd2a6972bcb9c46b96aa
Carlos Arevalo <carlosandresat@hotmail.com>
2025-07-21 21:26:46 -0500

    feat: add Table component and integrate it into ArenaGroupsStandings for displaying group results

--- components/arena-groups-standings.tsx
+++ components/arena-groups-standings.tsx
@@ -4,6 +4,15 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export function ArenaGroupsStandings() {
   return (
@@ -15,16 +24,184 @@ export function ArenaGroupsStandings() {
         <TabsTrigger value="group-d">Grupo D</TabsTrigger>
       </TabsList>
       <TabsContent value="group-a">
-        <p>Aquí los resultados del grupo A</p>
+        <Table>
+          <TableCaption>Click en cada equipo para ver más detalles</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-full">Equipo</TableHead>
+              <TableHead>Puntos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Equipo 1</TableCell>
+              <TableCell className="text-center">10</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 2</TableCell>
+              <TableCell className="text-center">8</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 3</TableCell>
+              <TableCell className="text-center">6</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 4</TableCell>
+              <TableCell className="text-center">4</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 5</TableCell>
+                <TableCell className="text-center">2</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell>Equipo 6</TableCell>
+                <TableCell className="text-center">0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 7</TableCell>
+              <TableCell className="text-center">0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 8</TableCell>
+              <TableCell className="text-center">0</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </TabsContent>
       <TabsContent value="group-b">
-        <p>Aquí los resultados del grupo B</p>
+        <Table>
+          <TableCaption>Click en cada equipo para ver más detalles</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-full">Equipo</TableHead>
+              <TableHead>Puntos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Equipo 1</TableCell>
+              <TableCell className="text-center">12</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 2</TableCell>
+              <TableCell className="text-center">11</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 3</TableCell>
+              <TableCell className="text-center">9</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 4</TableCell>
+              <TableCell className="text-center">8</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 5</TableCell>
+                <TableCell className="text-center">6</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell>Equipo 6</TableCell>
+                <TableCell className="text-center">4</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 7</TableCell>
+              <TableCell className="text-center">3</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 8</TableCell>
+              <TableCell className="text-center">0</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </TabsContent>
       <TabsContent value="group-c">
-        <p>Aquí los resultados del grupo C</p>
+        <Table>
+          <TableCaption>Click en cada equipo para ver más detalles</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-full">Equipo</TableHead>
+              <TableHead>Puntos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Equipo 1</TableCell>
+              <TableCell className="text-center">11</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 2</TableCell>
+              <TableCell className="text-center">9</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 3</TableCell>
+              <TableCell className="text-center">7</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 4</TableCell>
+              <TableCell className="text-center">6</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 5</TableCell>
+                <TableCell className="text-center">5</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell>Equipo 6</TableCell>
+                <TableCell className="text-center">3</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 7</TableCell>
+              <TableCell className="text-center">2</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 8</TableCell>
+              <TableCell className="text-center">1</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </TabsContent>
       <TabsContent value="group-d">
-        <p>Aquí los resultados del grupo D</p>
+        <Table>
+          <TableCaption>Click en cada equipo para ver más detalles</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-full">Equipo</TableHead>
+              <TableHead>Puntos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Equipo 1</TableCell>
+              <TableCell className="text-center">10</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 2</TableCell>
+              <TableCell className="text-center">8</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 3</TableCell>
+              <TableCell className="text-center">6</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 4</TableCell>
+              <TableCell className="text-center">4</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 5</TableCell>
+                <TableCell className="text-center">2</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell>Equipo 6</TableCell>
+                <TableCell className="text-center">0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 7</TableCell>
+              <TableCell className="text-center">0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Equipo 8</TableCell>
+              <TableCell className="text-center">0</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </TabsContent>
     </Tabs>
   )
