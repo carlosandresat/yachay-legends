f13b7826cb717f158bffeae501e6fa56ddc78ced
Carlos Arevalo <carlosandresat@hotmail.com>
2025-07-23 23:09:28 -0500

    feat: add Avatar component for player champions representation in TeamRow

--- components/team-row.tsx
+++ components/team-row.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function TeamRow() {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,45 +25,145 @@ export function TeamRow() {
                   <CardHeader>
                     <CardTitle className="text-sm">Match #1</CardTitle>
                   </CardHeader>
-                  <CardContent className="text-wrap">
-                    Aquí van los detalles del equipo, como jugadores,
-                    estadísticas, etc.
+                  <CardContent>
+                    <div className="flex items-center justify-evenly">
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Teemo.png"
+                            alt="Player1 Champion"
+                          />
+                          <AvatarFallback>P1</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 1</span>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Ashe.png"
+                            alt="Player2 Champion"
+                          />
+                          <AvatarFallback>P2</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 2</span>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader>
                     <CardTitle className="text-sm">Match #2</CardTitle>
                   </CardHeader>
-                  <CardContent className="text-wrap">
-                    Aquí van los detalles del equipo, como jugadores,
-                    estadísticas, etc.
+                  <CardContent>
+                    <div className="flex items-center justify-evenly">
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Teemo.png"
+                            alt="Player1 Champion"
+                          />
+                          <AvatarFallback>P1</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 1</span>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Ashe.png"
+                            alt="Player2 Champion"
+                          />
+                          <AvatarFallback>P2</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 2</span>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader>
                     <CardTitle className="text-sm">Match #3</CardTitle>
                   </CardHeader>
-                  <CardContent className="text-wrap">
-                    Aquí van los detalles del equipo, como jugadores,
-                    estadísticas, etc.
+                  <CardContent>
+                    <div className="flex items-center justify-evenly">
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Teemo.png"
+                            alt="Player1 Champion"
+                          />
+                          <AvatarFallback>P1</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 1</span>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Ashe.png"
+                            alt="Player2 Champion"
+                          />
+                          <AvatarFallback>P2</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 2</span>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader>
                     <CardTitle className="text-sm">Match #4</CardTitle>
                   </CardHeader>
-                  <CardContent className="text-wrap">
-                    Aquí van los detalles del equipo, como jugadores,
-                    estadísticas, etc.
+                  <CardContent>
+                    <div className="flex items-center justify-evenly">
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Teemo.png"
+                            alt="Player1 Champion"
+                          />
+                          <AvatarFallback>P1</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 1</span>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Ashe.png"
+                            alt="Player2 Champion"
+                          />
+                          <AvatarFallback>P2</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 2</span>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
                 <Card>
                   <CardHeader>
                     <CardTitle className="text-sm">Match #5</CardTitle>
                   </CardHeader>
-                  <CardContent className="text-wrap">
-                    Aquí van los detalles del equipo, como jugadores,
-                    estadísticas, etc.
+                  <CardContent>
+                    <div className="flex items-center justify-evenly">
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Teemo.png"
+                            alt="Player1 Champion"
+                          />
+                          <AvatarFallback>P1</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 1</span>
+                      </div>
+                      <div className="flex flex-col items-center">
+                        <Avatar className="size-10">
+                          <AvatarImage
+                            src="https://ddragon.leagueoflegends.com/cdn/15.14.1/img/champion/Ashe.png"
+                            alt="Player2 Champion"
+                          />
+                          <AvatarFallback>P2</AvatarFallback>
+                        </Avatar>
+                        <span className="text-sm">Jugador 2</span>
+                      </div>
+                    </div>
                   </CardContent>
                 </Card>
               </div>
